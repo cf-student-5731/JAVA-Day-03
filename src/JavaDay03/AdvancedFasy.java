fa78890b561ee2c40659841bfbc5a9b08b299525
fyzam <fyzam@fasy.at>
2020-07-01 13:49:38 +0200

    Advanced finished

---
+++ src/JavaDay03/AdvancedFasy.java
@@ -0,0 +1,32 @@
+package JavaDay03;
+
+public class AdvancedFasy {
+
+    public static void main(String[] args) {
+        InOutFasy y = new InOutFasy();
+
+//A6. Write a program that finds the maximum of 3 numbers (by using if/else). Discuss with
+//        the your partner.
+
+        y.out("Please enter the first Number: ");
+        float n1 = y.inputFloat();
+        y.out("Please enter the first Number: ");
+        float n2 = y.inputFloat();
+        y.out("Please enter the first Number: ");
+        float n3 = y.inputFloat();
+
+        if(n1 > n2){
+            n2 = n1;
+        }
+        if(n1 > n3){
+            n3 = n1;
+        }
+        if(n2 > n3){
+            y.outLn("The maximum is: " + n2);
+        }
+        else{
+            y.outLn("The maximum is: " + n3);
+        }
+
+    }
+}
